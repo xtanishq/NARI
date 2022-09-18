34fbf1e03450793889da647c79ef0f8bee8d109f
tanishq <tanishqkmr@gmail.com>
2022-09-18 20:18:05 +0530

    shake sensitivity increase

--- app/src/main/java/com/beast/nari/NariDashboard.java
+++ app/src/main/java/com/beast/nari/NariDashboard.java
@@ -42,7 +42,7 @@ public class NariDashboard extends AppCompatActivity {
 //                            z_accl > 2 ||
 //                            z_accl < -2)
 
-                    if (floatSum > 30){
+                    if (floatSum > 40){
                         Intent intent = new Intent(getApplicationContext(),sensorshake.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
