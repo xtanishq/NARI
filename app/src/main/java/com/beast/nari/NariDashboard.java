95bc314b86986a78035fe5d81f1051bf9ad627be
tanishq <tanishqkmr@gmail.com>
2022-09-18 10:12:22 +0530

    fake call+ helpline

--- app/src/main/java/com/beast/nari/NariDashboard.java
+++ app/src/main/java/com/beast/nari/NariDashboard.java
@@ -2,13 +2,50 @@ package com.beast.nari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NariDashboard extends AppCompatActivity {
-
+    ImageView msk,helpline;
+    ImageView fakecall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nari_dashboard);
+        msk = findViewById(R.id.msk);
+        fakecall = findViewById(R.id.fakecall);
+        helpline = findViewById(R.id.a);
+
+        msk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:181"));
+                startActivity(intent);
+            }
+        });
+        helpline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:181"));
+                startActivity(intent);
+
+            }
+        });
+        fakecall = findViewById(R.id.fakecall);
+
+        fakecall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NariDashboard.this, ring.class));
+
+            }
+        });
     }
 }
